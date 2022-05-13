b7d067a6ad7167b9d6ee50c1ca1724d95263dedb
Pete <pete@Petes-MacBook-Air.local>
2022-05-13 01:14:49 -0700

    use mysql2, add db to createConnection call

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const mysql = require('mysql')
-const dotenv = require('dotenv')
+const mysql = require('mysql2')
+const dotenv = require('dotenv').config()
 
 const PORT = process.env.port || 8000
 
@@ -8,6 +8,7 @@ const db = mysql.createConnection({
 	host: process.env.DB_HOST,
 	user: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
+	database: process.env.DB_DATABASE,
 })
 
 db.connect(err => {
