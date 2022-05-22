46627bb6e8c0ad9b92d9aa9b6289eb8ac92a0ffa
witaph <witaph@gmail.com>
2022-05-22 05:38:37 -0700

    create Images, Tags, ImageTags records upon successful addImage call

--- controller.js
+++ controller.js
@@ -1,6 +1,8 @@
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 const mysql = require('mysql2')
+const moment = require('moment')
+const util = require('util')
 
 const db = mysql.createConnection({
 	host: process.env.DB_HOST,
@@ -9,77 +11,136 @@ const db = mysql.createConnection({
 	database: process.env.DB_DATABASE,
 })
 
-const addImage = (req, res) => {
+const query = util.promisify(db.query).bind(db)
+
+const addImage = async (req, res) => {
 	console.log('POST /api/addImage req.body: ', req.body)
-	console.log('POST /api/addImage req.userName: ', req.userName)
+
+	if (!req.body.sourceURL || !req.body.sourceURL.length) {
+		return res.status(400).send({ message: 'must provide sourceURL' })
+	}
+
+	if (!req.body.dateCaptured || !req.body.dateCaptured.length) {
+		return res.status(400).send({ message: 'must provide dateCaptured (YYYY-MM-DD}' })
+	}
+
+	const dateCapturedMoment = moment(req.body.dateCaptured, 'YYYY-MM-DD')
+	if (!dateCapturedMoment.isValid()) {
+		return res.status(401).send({ message: 'dateCaptured must be in YYYY-MM-DD format' })
+	}
+
+	const imageValues = {
+		name: (req.body.name && req.body.name.length) ? req.body.name : null,
+		sourceURL: req.body.sourceURL,
+		sourceURL2: (req.body.sourceURL2 && req.body.sourceURL2.length) ? req.body.sourceURL2 : null,
+		state: (req.body.captureState && req.body.captureState.length) ? req.body.captureState : null,
+		country: (req.body.captureCountry && req.body.captureCountry.length) ? req.body.captureCountry : null,
+		dateCaptured: dateCapturedMoment.format('YYYY-MM-DD'),
+	}
+
+	const imageInsert = 'INSERT INTO Images (name, sourceURL, sourceURL2, state, country, dateCaptured)'
+		+ `VALUES ('${imageValues.name}', '${imageValues.sourceURL}', '${imageValues.sourceURL2}', '${imageValues.state}', '${imageValues.country}', '${imageValues.dateCaptured}')`
+
+	console.log('imageInsert: ', imageInsert)
+	
+	const imageInsertResult = await query(imageInsert)
+	console.log('imageInsertResult: ', imageInsertResult)
+
+	const imageTagRecords = await addTags(imageInsertResult.insertId, req.body.tags)
+	console.log('imageTagRecords: ', imageTagRecords)
+
+	const allTags = await query('SELECT * FROM Tags')
+	console.log('allTags: ', allTags)
+
+	res.send(allTags)
 }
 
-const getImages = (req, res) => {
-	console.log('GET /api/images')
-	const sql = 'SELECT * FROM Images'
-
-	db.query(sql, (err, results) => {
-		if (err) {
-			throw err
+const addTags = async (imageID, tags) => {
+	// insert new tags
+	const tagsWithIds = await Promise.all(tags.map(async tag => {
+		if (!tag.id) {
+			const tagInsert = `INSERT INTO Tags (tagText) VALUES ('${tag.name}')`
+			console.log('tag insert: ', tagInsert)
+			const tagInsertResult = await query(tagInsert)
+			return {
+				id: tagInsertResult.insertId,
+				name: tag.name,
+			}
+		} else {
+			return tag
 		}
+	}))
+	console.log('tagsWithIds: ', tagsWithIds)
 
-		console.log('GET /api/images results: ', results)
-		res.send(results)
-	})
+	const imageTags = await query(`SELECT * FROM ImageTags WHERE imageID = ${imageID}`)
+	const existingImageTagIds = imageTags.map(tag => tag.tagID)
+
+	// create ImageTags records for any tags not already applied to the image
+	await Promise.all(tagsWithIds.map(async tag => {
+		console.log('tag: ', tag)
+		if (!existingImageTagIds.includes(tag.id)) {
+			const imageTagInsert = `INSERT INTO ImageTags (imageID, tagID) VALUES (${imageID}, ${tag.id})`
+			console.log('imageTagInsert: ', imageTagInsert)
+			const imageTagInsertResult = await query(imageTagInsert)
+			imageTags.push({
+				imageID,
+				tagID: tag.id,
+			})
+		}
+	}))
+
+	return imageTags
 }
 
-const login = (req, res) => {
+const getImages = async (req, res) => {
+	console.log('GET /api/images')
+	const imageSelect = 'SELECT * FROM Images'
+
+	const images = await query(imageSelect)
+	console.log('GET /api/images results: ', images)
+	res.send(images)
+}
+
+const login = async (req, res) => {
 	console.log('POST /api/login req.body: ', req.body)
 	// console.log('POST /api/login res: ', Object.getOwnPropertyNames(res))
 	if (!req.body.userName) {
 		return res.status(400).send({ message: 'must provide userName for login' })
 	}
 
-	const sql = `SELECT * FROM Users WHERE userName = '${req.body.userName}'`
+	const userSelect = `SELECT * FROM Users WHERE userName = '${req.body.userName}'`
 
-	db.query(sql, (err, results) => {
-		if (err) {
-			console.log('POST /api/login Users select err: ', err)
-			throw err
-		}
+	const users = await query(userSelect)
+	console.log('POST /api/login Users select results: ', users)
 
-		console.log('POST /api/login Users select results: ', results)
+	if (!users || !users.length) {
+		return res.status(404).send({ message: 'user not found' })
+	}
 
-		if(!results || !results.length) {
-			return res.status(404).send({ message: 'user not found' })
-		}
+	const passwordIsValid = bcrypt.compareSync(req.body.password, users[0].password)
+	if (!passwordIsValid) {
+		return res.status(401).send({ message: 'invalid password' })
+	}
 
-		const passwordIsValid = bcrypt.compareSync(req.body.password, results[0].password)
-		if (!passwordIsValid) {
-			return res.status(401).send({ message: 'invalid password' })
-		}
+	const token = jwt.sign(
+		{ id: users[0].userName },
+		process.env.JWT_SECRET,
+		{ expiresIn: 86400 } // 24 hrs
+	)
 
-		const token = jwt.sign(
-			{ id: results[0].userName },
-			process.env.JWT_SECRET,
-			{ expiresIn: 86400 } // 24 hrs
-		)
-
-		res.status(200).send({
-			accessToken: token
-		})
+	res.status(200).send({
+		accessToken: token
 	})
 }
 
-const getTags = (req, res) => {
+const getTags = async (req, res) => {
 	console.log('GET /api/tags')
-	const sql = 'SELECT * FROM Tags'
+	const tagSelect = 'SELECT * FROM Tags'
 
-	db.query(sql, (err, results) => {
-		if (err) {
-			throw err
-		}
+	const tags = await query(tagSelect)
 
-		console.log('GET /api/tags results: ', results)
-		res.send({
-			tags: JSON.stringify(results)
-		})
-	})
+	console.log('GET /api/tags results: ', tags)
+	res.send(tags)
 }
 
 module.exports = {
